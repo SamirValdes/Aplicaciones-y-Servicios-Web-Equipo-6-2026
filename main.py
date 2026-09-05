8c3e678c0e63cabb163236e6012b27360a505470
vilmar2008 <vilmarrivas1137168@correo.itm.edu.co>
2026-09-04 19:15:02 -0500

    feat(persona): incluye router de persona en main.py

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
-from contextlib import asynccontextmanager
-
-from fastapi import FastAPI
-
-
+﻿from fastapi import FastAPI
 from src.database.database import Base, engine
+from src.api.estudiante import router as estudiante_router
+from src.api.persona import router as persona_router
 
+Base.metadata.create_all(bind=engine)
 
-from src.api.estudiante import router as estudiantes_router
-from src.entities.estudiante import Estudiante
+app = FastAPI(title="API Equipo 6")
 
-
-@asynccontextmanager
-async def lifespan(_app: FastAPI):
-
-    Base.metadata.create_all(bind=engine)
-    yield
-
-
-app = FastAPI(
-    title="API Estudiantes - ITM 2026-2",
-    description="API REST con FastAPI, SQLAlchemy y Neon PostgreSQL",
-    version="1.0.0",
-    lifespan=lifespan,
-)
-
+app.include_router(estudiante_router)
+app.include_router(persona_router)
 
 @app.get("/")
-def inicio():
-    return {
-        "mensaje": "API  - Aplicaciones y servicios web ITM 2026-2",
-        "docs": "/docs",
-    }
-
-
-app.include_router(estudiantes_router)
+def read_root():
+    return {"message": "API activa"}
