4133eb2c33d41de08b515700d00d3b38ba040b69
vilmar2008 <vilmarrivas1137168@correo.itm.edu.co>
2026-09-18 15:49:07 -0500

    feat: agrega CI/CD y seeder independientes para Vilmar

---
+++ src/database/seeder_persona.py
@@ -0,0 +1,22 @@
+﻿from sqlalchemy.orm import Session
+from src.database.database import engine, Base
+from src.entities.persona import Persona
+
+def seed_data():
+    Base.metadata.create_all(bind=engine)
+    
+    with Session(engine) as session:
+        if not session.query(Persona).first():
+            print("Insertando datos iniciales de Persona...")
+            nueva_persona = Persona(
+                nombre="Vilmar",
+                apellido="Rivas"
+            )
+            session.add(nueva_persona)
+            session.commit()
+            print("Seeder de Persona ejecutado con exito.")
+        else:
+            print("Los datos de Persona ya existen.")
+
+if __name__ == "__main__":
+    seed_data()
