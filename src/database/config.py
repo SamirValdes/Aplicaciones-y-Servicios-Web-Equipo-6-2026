bf3fbd3fb2b12b2beae674eeb52842a1a93959f2
juandavidLDS <juandavidweed2@gmail.com>
2026-09-17 22:25:51 -0500

    feat: update configuration to load environment variables using dotenv

--- src/database/config.py
+++ src/database/config.py
@@ -1,6 +1,11 @@
 """Módulo de configuración de variables de entorno y conexión"""
 
+import os
+
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv(override=True)
 
 
 class Settings(BaseSettings):
